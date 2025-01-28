e829127fa2eb5b260baa3792a4deda98b37d2c30
Rafael Escribano <rescriba@proton.me>
2025-01-28 12:33:44 -0500

    style: add mobile responsiveness to Link List block
    - Added media query for mobile devices
    - Adjusted inline padding for smaller screens
    - Updated both source and build CSS files

--- build/link-list/index.asset.php
+++ build/link-list/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'aae30d8ad79c71a4fabb');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '61a0773482c1baf321d2');
