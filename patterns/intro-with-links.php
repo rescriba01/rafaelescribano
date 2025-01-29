39e03e759ddcaff2c21f4d7e4f184ba06f5e7bb9
Rafael Escribano <rescriba@proton.me>
2025-01-29 13:20:22 -0500

    refactor: update Intro with Links pattern structure and metadata
    - Added wrapping section group for improved semantic structure
    - Updated column and heading class names
    - Modified link list block title to "Project Links"
    - Maintained existing content and layout

--- patterns/intro-with-links.php
+++ patterns/intro-with-links.php
@@ -9,9 +9,10 @@
  */
 ?>
 
-<!-- wp:columns {"metadata":{"categories":["featured"],"patternName":"re/intro-with-links","name":"Intro with Links"},"align":"full","className":"intro-with-links"} -->
-<div class="wp-block-columns alignfull intro-with-links"><!-- wp:column {"width":"66.66%","metadata":{"name":"Introduction"},"className":"introduction"} -->
-<div class="wp-block-column introduction" style="flex-basis:66.66%"><!-- wp:heading {"level":1,"className":"wp-block-heading is-style-display"} -->
+<!-- wp:group {"tagName":"section","metadata":{"name":"Intro-group"},"align":"full","className":"intro-group","layout":{"type":"constrained"}} -->
+<section class="wp-block-group alignfull intro-group"><!-- wp:columns {"metadata":{"categories":["featured"],"patternName":"re/intro-with-links","name":"Intro-columns"},"align":"full","className":"intro-with-links-coulmns"} -->
+<div class="wp-block-columns alignfull intro-with-links-coulmns"><!-- wp:column {"width":"66.66%","metadata":{"name":"Introduction"},"className":"introduction"} -->
+<div class="wp-block-column introduction" style="flex-basis:66.66%"><!-- wp:heading {"level":1,"className":"is-style-display"} -->
 <h1 class="wp-block-heading is-style-display">I'm Rafael Escribano</h1>
 <!-- /wp:heading -->
 
@@ -21,8 +22,9 @@
 <!-- /wp:column -->
 
 <!-- wp:column {"width":"28%","metadata":{"name":"Links"},"className":"project-links"} -->
-<div class="wp-block-column project-links" style="flex-basis:28%"><!-- wp:re/link-list {"links":[{"text":"MattressClarity","url":"#"},{"text":"Payper","url":"#"},{"text":"Vipline","url":"#"},{"text":"Metafy","url":"#"},{"text":"Fleetsu","url":"#"}]} -->
-<div class="wp-block-re-link-list"><h3 class="link-list-title">Links</h3><ul class="link-list"><li><a href="#">MattressClarity</a></li><li><a href="#">Payper</a></li><li><a href="#">Vipline</a></li><li><a href="#">Metafy</a></li><li><a href="#">Fleetsu</a></li></ul></div>
+<div class="wp-block-column project-links" style="flex-basis:28%"><!-- wp:re/link-list {"title":"Project Links","links":[{"text":"MattressClarity","url":"#"},{"text":"Payper","url":"#"},{"text":"Vipline","url":"#"},{"text":"Metafy","url":"#"},{"text":"Fleetsu","url":"#"}]} -->
+<div class="wp-block-re-link-list"><h3 class="link-list-title">Project Links</h3><ul class="link-list"><li><a href="#">MattressClarity</a></li><li><a href="#">Payper</a></li><li><a href="#">Vipline</a></li><li><a href="#">Metafy</a></li><li><a href="#">Fleetsu</a></li></ul></div>
 <!-- /wp:re/link-list --></div>
 <!-- /wp:column --></div>
-<!-- /wp:columns -->
+<!-- /wp:columns --></section>
+<!-- /wp:group -->
