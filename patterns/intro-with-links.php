783ad2798b04536504c72dd3d2c3124ee24054c3
Rafael Escribano <rescriba@proton.me>
2025-02-10 22:31:33 -0500

    refactor(intro-with-links): update project links and fix typo in column class

--- patterns/intro-with-links.php
+++ patterns/intro-with-links.php
@@ -10,8 +10,8 @@
 ?>
 
 <!-- wp:group {"tagName":"section","metadata":{"name":"Intro-group"},"align":"full","className":"intro-group","layout":{"type":"constrained"}} -->
-<section class="wp-block-group alignfull intro-group"><!-- wp:columns {"metadata":{"categories":["featured"],"patternName":"re/intro-with-links","name":"Intro-columns"},"align":"full","className":"intro-with-links-coulmns"} -->
-<div class="wp-block-columns alignfull intro-with-links-coulmns"><!-- wp:column {"width":"66.66%","metadata":{"name":"Introduction"},"className":"introduction"} -->
+<section class="wp-block-group alignfull intro-group"><!-- wp:columns {"metadata":{"categories":["featured"],"patternName":"re/intro-with-links","name":"Intro-columns"},"align":"full","className":"intro-with-links-columns"} -->
+<div class="wp-block-columns alignfull intro-with-links-columns"><!-- wp:column {"width":"66.66%","metadata":{"name":"Introduction"},"className":"introduction"} -->
 <div class="wp-block-column introduction" style="flex-basis:66.66%"><!-- wp:heading {"level":1,"className":"is-style-display"} -->
 <h1 class="wp-block-heading is-style-display">I'm Rafael Escribano</h1>
 <!-- /wp:heading -->
@@ -22,8 +22,8 @@
 <!-- /wp:column -->
 
 <!-- wp:column {"width":"28%","metadata":{"name":"Links"},"className":"project-links"} -->
-<div class="wp-block-column project-links" style="flex-basis:28%"><!-- wp:re/link-list {"title":"Project Links","links":[{"text":"MattressClarity","url":"#"},{"text":"Payper","url":"#"},{"text":"Vipline","url":"#"},{"text":"Metafy","url":"#"},{"text":"Fleetsu","url":"#"}]} -->
-<div class="wp-block-re-link-list"><h3 class="link-list-title">Project Links</h3><ul class="link-list"><li><a href="#">MattressClarity</a></li><li><a href="#">Payper</a></li><li><a href="#">Vipline</a></li><li><a href="#">Metafy</a></li><li><a href="#">Fleetsu</a></li></ul></div>
+<div class="wp-block-column project-links" style="flex-basis:28%"><!-- wp:re/link-list {"title":"Project Links","links":[{"text":"Pillar4 Media","url":"#"},{"text":"Saltwater Collective","url":"#"},{"text":"Personal Projects","url":"#"}]} -->
+<div class="wp-block-re-link-list"><h3 class="link-list-title">Project Links</h3><ul class="link-list"><li><a href="#">Pillar4 Media</a></li><li><a href="#">Saltwater Collective</a></li><li><a href="#">Personal Projects</a></li></ul></div>
 <!-- /wp:re/link-list --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></section>
