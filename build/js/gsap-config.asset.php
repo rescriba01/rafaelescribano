05e33ffb23973b1278e6aaa9c5e7484f06270378
Rafael Escribano <rescriba@proton.me>
2025-02-18 11:51:59 -0500

    feat(gutenberg-block): add Link List block with advanced editing and styling
    Implement a new Gutenberg block for creating customizable link lists with:
    - Dynamic link text and URL editing
    - Hover effects and responsive styling
    - Editor and frontend CSS for consistent design
    - Block configuration with flexible attributes

---
+++ build/js/gsap-config.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '8f370fac60076c5f3212');
---
+++ build/vendors/gsap.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '1a27fd503936473e8648');
