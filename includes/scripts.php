6abec302ebf828e02c54eb47694d407250ff9a7c
Rafael Escribano <rescriba@proton.me>
2025-02-18 21:40:07 -0500

    refactor(scripts): remove navigation script from theme enqueuing

--- includes/scripts.php
+++ includes/scripts.php
@@ -14,14 +14,6 @@ if (!defined('ABSPATH')) {
  * Enqueue theme scripts
  */
 function re_enqueue_scripts() {
-    wp_enqueue_script(
-        're-navigation',
-        RE_THEME_URL . 'assets/js/navigation.js',
-        array(),
-        RE_THEME_VERSION,
-        true
-    );
-
     // Pattern: Work Card Stack
     wp_enqueue_script(
         're-pattern-work-card-stack',
